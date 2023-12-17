ac9144ca5958614fbfcfc1f664e5ab8dfa165eeb
Tu Tran Le <tranletu2003@gmail.com>
2023-12-17 15:33:40 +0700

    add-required-to-input

--- resources/views/flight/create.blade.php
+++ resources/views/flight/create.blade.php
@@ -10,49 +10,49 @@
         <div class="col">
             <div class="form-group mb-2">
                 <label for="txtFrom">Nơi đi</label>
-                <input type="text" class="form-control" name="From" id="txtFrom" placeholder="">
+                <input type="text" class="form-control" name="From" id="txtFrom" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtTo">Nơi đến</label>
-                <input type="text" class="form-control" name="To" id="txtTo" placeholder="">
+                <input type="text" class="form-control" name="To" id="txtTo" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtDate">Ngày bay</label>
-                <input type="date" class="form-control" name="Date" id="txtDate" placeholder="">
+                <input type="date" class="form-control" name="Date" id="txtDate" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtDepartureTime">Giờ khởi hành</label>
-                <input type="time" class="form-control" name="DepartureTime" id="txtDepartureTime" placeholder="">
+                <input type="time" class="form-control" name="DepartureTime" id="txtDepartureTime" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtArrivalTime">Giờ đến</label>
-                <input type="time" class="form-control" name="ArrivalTime" id="txtArrivalTime" placeholder="">
+                <input type="time" class="form-control" name="ArrivalTime" id="txtArrivalTime" placeholder="" required>
             </div>
             <div class="form-group">
                 <label for="txtPrice">Giá vé</label>
-                <input type="text" class="form-control" name="Price" id="txtPrice" placeholder="">
+                <input type="text" class="form-control" name="Price" id="txtPrice" placeholder="" required>
             </div>
         </div>
         <div class="col d-flex flex-column">
             <div class="form-group mb-2">
                 <label for="txtTravelTime">Thời gian bay</label>
-                <input type="text" class="form-control" name="TravelTime" id="txtTravelTime" placeholder="">
+                <input type="text" class="form-control" name="TravelTime" id="txtTravelTime" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtStop">Trạm dừng</label>
-                <input type="text" class="form-control" name="StopDirect" id="txtStop" placeholder="">
+                <input type="text" class="form-control" name="StopDirect" id="txtStop" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtName">Tên hãng bay</label>
-                <input type="text" class="form-control" name="Name" id="txtName" placeholder="">
+                <input type="text" class="form-control" name="Name" id="txtName" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtSeatClass">Hạng ghế</label>
-                <input type="text" class="form-control" name="SeatClass" id="txtSeatClass" placeholder="">
+                <input type="text" class="form-control" name="SeatClass" id="txtSeatClass" placeholder="" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtNumSeat">Số ghế</label>
-                <input type="text" class="form-control" name="NumSeat" id="txtNumSeat" placeholder="">
+                <input type="text" class="form-control" name="NumSeat" id="txtNumSeat" placeholder="" required>
             </div>
             <div class="input-group-btn mt-auto">
                 <button class="btn btn-success fw-bold w-25" type="submit">Thêm chuyến bay</button>
--- resources/views/flight/edit.blade.php
+++ resources/views/flight/edit.blade.php
@@ -11,58 +11,58 @@
             <div class="form-group mb-2">
                 <label for="txtFrom">Nơi đi</label>
                 <input type="text" class="form-control" name="From" id="txtFrom" placeholder=""
-                    value="{{$flight->From}}">
+                    value="{{$flight->From}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtTo">Nơi đến</label>
-                <input type="text" class="form-control" name="To" id="txtTo" placeholder="" value="{{$flight->To}}">
+                <input type="text" class="form-control" name="To" id="txtTo" placeholder="" value="{{$flight->To}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtDate">Ngày bay</label>
                 <input type="date" class="form-control" name="Date" id="txtDate" placeholder=""
-                    value="{{$flight->Date}}">
+                    value="{{$flight->Date}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtDepartureTime">Giờ khởi hành</label>
                 <input type="time" class="form-control" name="DepartureTime" id="txtDepartureTime" placeholder=""
-                    value="{{$flight->DepartureTime}}">
+                    value="{{$flight->DepartureTime}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtArrivalTime">Giờ đến</label>
                 <input type="time" class="form-control" name="ArrivalTime" id="txtArrivalTime" placeholder=""
-                    value="{{$flight->ArrivalTime}}">
+                    value="{{$flight->ArrivalTime}}" required>
             </div>
             <div class="form-group">
                 <label for="txtPrice">Giá vé</label>
                 <input type="text" class="form-control" name="Price" id="txtPrice" placeholder=""
-                    value="{{$flight->Price}}">
+                    value="{{$flight->Price}}" required>
             </div>
         </div>
         <div class="col d-flex flex-column">
             <div class="form-group mb-2">
                 <label for="txtTravelTime">Thời gian bay</label>
                 <input type="text" class="form-control" name="TravelTime" id="txtTravelTime" placeholder=""
-                    value="{{$flight->TravelTime}}">
+                    value="{{$flight->TravelTime}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtStop">Trạm dừng</label>
                 <input type="text" class="form-control" name="Stop/Direct" id="txtStop" placeholder=""
-                    value="{{$flight['Stop/Direct']}}">
+                    value="{{$flight['Stop/Direct']}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtName">Tên hãng bay</label>
                 <input type="text" class="form-control" name="Name" id="txtName" placeholder=""
-                    value="{{$flight->Name}}">
+                    value="{{$flight->Name}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtSeatClass">Hạng ghế</label>
                 <input type="text" class="form-control" name="SeatClass" id="txtSeatClass" placeholder=""
-                    value="{{$flight->SeatClass}}">
+                    value="{{$flight->SeatClass}}" required>
             </div>
             <div class="form-group mb-2">
                 <label for="txtNumSeat">Số ghế</label>
                 <input type="text" class="form-control" name="NumSeat" id="txtNumSeat" placeholder=""
-                    value="{{$flight->NumSeat}}">
+                    value="{{$flight->NumSeat}}" required>
             </div>
             <div class="input-group-btn mt-auto">
                 <button id="update-btn" class="btn btn-success w-25 fw-bold" type="submit">Cập Nhật</button>
